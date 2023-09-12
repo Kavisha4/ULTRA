061d30e2e93079fd3d9484a86a14a8f3762e4a85
Kavisha4 <kvsha2002@gmail.com>
2023-09-12 18:10:02 +0530

    question2

--- Runnables/question2.cpp
+++ Runnables/question2.cpp
@@ -1,147 +1,99 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 #include <queue>
-#include <algorithm>
-#include <sstream>
-#include <string>
+#include <climits>
 
-// Define the Node structure to represent nodes in the grid
-struct Node {
-    int x, y;     // Coordinates
-    int value;    // Weight or cost
-    bool blocked; // Indicates if the node is blocked or not
+using namespace std;
 
-    Node(int x, int y, int value, bool blocked = false) : x(x), y(y), value(value), blocked(blocked) {}
+// Define a structure to represent an edge in the graph
+struct Edge {
+    int v;      // Destination vertex
+    int w;      // Weight of the edge
 };
 
-// Define a custom comparator for nodes in the priority queue
-struct CompareNodes {
-    bool operator()(const Node& a, const Node& b) const {
-        return a.value > b.value;
-    }
-};
-
-// Function to initialize the grid from a DIMACS-style file
-bool initializeGridFromFile(const std::string& filename, std::vector<std::vector<Node>>& grid, Node& start, Node& goal) {
-    std::ifstream file(filename);
-
-    if (!file.is_open()) {
-        std::cerr << "Error: Unable to open the file " << filename << std::endl;
-        return false;
+// Function to read the .gr file and create a graph representation
+vector<vector<Edge>> readGraph(const string& filename, int& n, int& m) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Error: Unable to open the file " << filename << endl;
+        exit(1);
     }
 
-    std::string line;
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        char type;
-        int x, y, value;
+    char type;
+    file >> type >> n >> m;
 
-        if (!(iss >> type)) continue; // Skip empty lines
+    vector<vector<Edge>> graph(n);
 
-        if (type == 'c') continue; // Skip comment lines
+    while (!file.eof()) {
+        char c;
+        int u, v, w;
+        file >> c;
 
-        if (type == 'p') {
-            // Parse problem line (p sp numNodes numEdges)
-            std::string problemType;
-            int numNodes, numEdges;
-            if (!(iss >> problemType >> numNodes >> numEdges)) {
-                std::cerr << "Error: Invalid problem line format." << std::endl;
-                return false;
-            }
-            // Resize the grid based on the number of nodes (adjust as needed)
-            grid.resize(numNodes, std::vector<Node>(numNodes, Node(0, 0, 0, false)));
-        } else if (type == 'a') {
-            // Parse edge descriptor (a u v w)
-            if (!(iss >> x >> y >> value)) {
-                std::cerr << "Error: Invalid edge descriptor format." << std::endl;
-                return false;
-            }
-            // Set the weight in the grid (you can adjust this as needed)
-            grid[x - 1][y - 1] = Node(x - 1, y - 1, value);
+        if (c == 'a') {
+            file >> u >> v >> w;
+            graph[u - 1].push_back({v - 1, w});
         }
     }
 
-    // Set the start and goal nodes (you can adjust this as needed)
-    start = grid[0][0];
-    goal = grid[grid.size() - 1][grid[0].size() - 1];
-
-    return true;
+    file.close();
+    return graph;
 }
 
-// A* search algorithm
-int astarSearch(const std::vector<std::vector<Node>>& grid, const Node& start, const Node& goal) {
-    const int numRows = grid.size();
-    const int numCols = grid[0].size();
+// Modified Dijkstra's algorithm to find the shortest path
+vector<int> dijkstra(const vector<vector<Edge>>& graph, int source) {
+    int n = graph.size();
+    vector<int> dist(n, INT_MAX);
+    dist[source] = 0;
 
-    // Create a 2D vector to store the cost-so-far
-    std::vector<std::vector<int>> costSoFar(numRows, std::vector<int>(numCols, INT_MAX));
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, source});
 
-    // Create a priority queue for open nodes
-    std::priority_queue<Node, std::vector<Node>, CompareNodes> openSet;
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        int d = pq.top().first;
+        pq.pop();
 
-    // Initialize the starting node
-    costSoFar[start.x][start.y] = 0;
-    openSet.push(start);
+        if (d > dist[u]) continue;
 
-    while (!openSet.empty()) {
-        // Get the node with the lowest cost from the open set
-        Node current = openSet.top();
-        openSet.pop();
-
-        if (current.x == goal.x && current.y == goal.y) {
-            // Reached the goal
-            return costSoFar[goal.x][goal.y];
-        }
-
-        // Define possible neighbor directions (up, down, left, right)
-        const int dx[] = {0, 0, -1, 1};
-        const int dy[] = {-1, 1, 0, 0};
-
-        for (int i = 0; i < 4; ++i) {
-            int newX = current.x + dx[i];
-            int newY = current.y + dy[i];
-
-            // Check if the neighbor is within bounds
-            if (newX >= 0 && newX < numRows && newY >= 0 && newY < numCols) {
-                const Node& neighbor = grid[newX][newY];
-
-                if (!neighbor.blocked) {
-                    int newCost = costSoFar[current.x][current.y] + neighbor.value;
-
-                    if (newCost < costSoFar[newX][newY]) {
-                        costSoFar[newX][newY] = newCost;
-                        openSet.push(Node(newX, newY, newCost));
-                    }
-                }
+        for (const Edge& edge : graph[u]) {
+            int v = edge.v;
+            int w = edge.w;
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
             }
         }
     }
 
-    // If the goal is unreachable
-    return -1;
+    return dist;
 }
 
 int main() {
-    // Define the filename for the DIMACS-style graph file
-    std::string filename = "your_graph.gr";
+    int n, m;
+vector<vector<Edge>> graph = readGraph(R"(C:\CiSTup\ULTRA\Runnables\USA-road-d.FLA.gr)", n, m);
 
-    // Initialize the grid and start/goal nodes from the file
-    std::vector<std::vector<Node>> grid;
-    Node start, goal;
-
-    if (!initializeGridFromFile(filename, grid, start, goal)) {
-        return 1; // Exit with an error code
+    // Generate 200 random source-destination pairs
+    srand(time(nullptr));
+    vector<pair<int, int>> randomPairs;
+    for (int i = 0; i < 200; ++i) {
+        int source = rand() % n;
+        int dest = rand() % n;
+        randomPairs.push_back({source, dest});
     }
 
-    // Perform A* search
-    int result = astarSearch(grid, start, goal);
-
-    if (result != -1) {
-        std::cout << "Shortest path cost: " << result << std::endl;
-    } else {
-        std::cout << "Goal is unreachable." << std::endl;
+    // Calculate the total runtime for all 200 pairs
+    clock_t start = clock();
+    for (const pair<int, int>& pair : randomPairs) {
+        vector<int> shortestPath = dijkstra(graph, pair.first);
     }
+    clock_t end = clock();
+
+    // Print the total runtime in seconds
+    double totalRuntime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    cout << "Total runtime in seconds for 200 random modified Dijkstra: " << totalRuntime << endl;
 
     return 0;
 }
